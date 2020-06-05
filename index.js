0fc12c57f81709aca8e74d047225d36991fddffc
Simon Ellett <simon.ellett@technocloud-software.co.uk>
2020-06-05 13:08:33 +0100

    SIS-10830 altering index imports due to errors seen

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import { ApiService } from './api-services/api.service'
-import { FoundationsApiService } from './api-services/foundations-api.service'
+// Note: done this way otherwise errors are seen in consuming application
+const ApiService = require('./api-services/api.service')
+const FoundationsApiService = require('./api-services/foundations-api.service')
 
 module.exports = {
     ApiService,
