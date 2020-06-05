f5a29ef263b17878a35c9fcd8ab1d26ea46edd02
Simon Ellett <simon.ellett@technocloud-software.co.uk>
2020-06-05 12:44:35 +0100

    SIS10830 Adding index js for npm entry point

---
+++ index.js
@@ -0,0 +1,7 @@
+import { ApiService } from '../api-services/api.service'
+import { FoundationsApiService } from '../api-services/foundations-api.service'
+
+module.exports = {
+    ApiService,
+    FoundationsApiService
+}
