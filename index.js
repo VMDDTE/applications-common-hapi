5fcb0086b7b993121dd17cb48e061dc9e6cf2f3b
Simon Ellett <simon.ellett@technocloud-software.co.uk>
2020-06-11 13:09:13 +0100

    SIS-10833 Exposing protective monitoring service in npm package

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { ApiService } from './api-services/api.service'
 import { FoundationsApiService } from './api-services/foundations-api.service'
+import { ProtectiveMonitoringService } from './services/protective-monitoring.service'
 
 export {
     ApiService,
-    FoundationsApiService
+    FoundationsApiService,
+    ProtectiveMonitoringService
 }
