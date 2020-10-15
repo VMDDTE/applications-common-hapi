87209c11142fdd372200f8683b00fa6b72ee7dfd
Simon Ellett <simon.ellett@technocloud-software.co.uk>
2020-10-15 17:01:27 +0100

    DEV-12696 Updating helper checks

--- common/logging.helpers.js
+++ common/logging.helpers.js
@@ -2,8 +2,8 @@ import { extractLogMessageInfoFromHapiRequest, isHealthCheckRequest, isResourceR
 
 // Decide if this is better placed in logging?
 export function isTypeOfVmdLogger (vmdLogger) {
-    // We dont really care what type of logger is provided as long is it supports logStandardDebug/logStandardInfo/logStandardError
-    if (typeof vmdLogger.logStandardDebug !== 'function' || typeof vmdLogger.logStandardInfo !== 'function' || typeof vmdLogger.logStandardError !== 'function') {
+    // We dont really care what type of logger is provided as long is it supports logRequestDebug/logRequestInfo/logRequestError
+    if (typeof vmdLogger.logRequestDebug !== 'function' || typeof vmdLogger.logRequestInfo !== 'function' || typeof vmdLogger.logRequestError !== 'function') {
         throw new Error('VmdLogger does not provide required methods')
     }
 }
